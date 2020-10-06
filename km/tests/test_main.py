bf86fca021641f366fd9bb668b4acae2a1dd264c
eric audemard <eric.audemard@umontreal.ca>
2020-10-06 16:07:29 -0400

    fix test

--- km/tests/test_main.py
+++ km/tests/test_main.py
@@ -614,37 +614,37 @@ class TestkmMuttaion(unittest.TestCase):
                          "315",
                          "Test fail: min_cov1 -> wronng kmer_nb_0")
 
-        report_line = report_output[3].split("\t")
+        report_line2 = report_output[3].split("\t")
 
-        self.assertEqual(report_line[0],
+        self.assertEqual(report_line2[0],
                          "./data/jf/03H112_IandI.jf",
                          "Test fail: min_cov2 -> wrong DB")
 
-        self.assertEqual(report_line[1],
+        self.assertEqual(report_line2[1],
                          "275596",
                          "Test fail: min_cov2 -> wronng count")
 
-        self.assertEqual(report_line[2],
+        self.assertEqual(report_line2[2],
                          "345",
                          "Test fail: min_cov2 -> wronng length")
 
-        self.assertEqual(report_line[3],
+        self.assertEqual(report_line2[3],
                          "618",
                          "Test fail: min_cov2 -> wronng min")
 
-        self.assertEqual(report_line[4],
+        self.assertEqual(report_line2[4],
                          "1368",
                          "Test fail: min_cov2 -> wronng max")
 
-        self.assertEqual(report_line[5],
+        self.assertEqual(report_line2[5],
                          "874.91",
                          "Test fail: min_cov2 -> wronng mean")
 
-        self.assertEqual(report_line[6],
+        self.assertEqual(report_line2[6],
                          "315",
                          "Test fail: min_cov2 -> wronng kmer_nb")
 
-        self.assertEqual(report_line[7],
+        self.assertEqual(report_line2[7],
                          "0",
                          "Test fail: min_cov2 -> wronng kmer_nb_0")
 
