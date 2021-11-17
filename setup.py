6247925acee5e3d5bfdb21e4218dba7bec963a40
Albert Feghaly <albert.feghaly@umontreal.ca>
2021-11-16 23:01:10 -0500

    Using setup_requires causes problems with pip

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='2.1.0',
+    version='2.1.1',
 
     description='A software for RNA-seq investigation using k-mer decomposition',
     long_description=long_description,
@@ -75,7 +75,6 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    setup_requires=['numpy>=1.18.1', 'matplotlib>=3.4.2', 'pyjellyfish>=1.0.0', 'pytest>=6.2.0'],
     install_requires=['numpy>=1.18.1', 'matplotlib>=3.4.2', 'pyjellyfish>=1.0.0', 'pytest>=6.2.0'],
     python_requires='>=3.6',
 
